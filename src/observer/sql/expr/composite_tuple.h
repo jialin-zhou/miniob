08afecc5e9928777411af2842c5cfab804c375ac
jialin.zhou <1597709078@qq.com>
2025-06-07 19:53:43 +0800

    fix bug: Implement the clone function for Tuple's inherited class

--- src/observer/sql/expr/composite_tuple.h
+++ src/observer/sql/expr/composite_tuple.h
@@ -15,6 +15,8 @@ See the Mulan PSL v2 for more details. */
 #pragma once
 
 #include "sql/expr/tuple.h"
+#include <vector>
+#include <memory> // For std::unique_ptr
 
 /**
  * @brief 组合的Tuple
@@ -26,6 +28,30 @@ class CompositeTuple : public Tuple
 public:
   CompositeTuple()          = default;
   virtual ~CompositeTuple() = default;
+
+      /**
+     * @brief 创建并返回当前 CompositeTuple 的一个深拷贝副本。
+     * @details
+     * CompositeTuple 内部包含一个 Tuple 列表。克隆时，需要对这个列表中的
+     * 每一个 Tuple 对象也进行克隆，以创建完全独立的副本。
+     * @return 指向新创建的元组副本的 unique_ptr；如果失败则返回 nullptr。
+     */
+    std::unique_ptr<Tuple> clone() const override {
+        // 创建一个新的 CompositeTuple 对象
+        auto cloned_composite = std::make_unique<CompositeTuple>();
+        
+        // CompositeTuple 的核心是其内部的元组列表（假设名为 tuples_）。
+        // 假设成员是: std::vector<std::unique_ptr<Tuple>> tuples_;
+        cloned_composite->tuples_.reserve(this->tuples_.size());
+        for (const auto& child_tuple_ptr : this->tuples_) {
+            if (child_tuple_ptr) {
+                cloned_composite->add_tuple(child_tuple_ptr->clone());
+            } else {
+                cloned_composite->add_tuple(nullptr);
+            }
+        }
+        return cloned_composite;
+    }
 
   /// @brief 删除默认构造函数
   CompositeTuple(const CompositeTuple &) = delete;
